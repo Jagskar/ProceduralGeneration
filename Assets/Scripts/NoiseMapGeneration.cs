2a81c3e77abd40a9015fdae2c12c728dca8887d6
Jags <jags.saredia@cadju.com>
2022-05-25 20:29:19 +0100

    Creating NoiseMapGeneration script

--- Assets/Scripts/NoiseMapGeneration.cs
+++ Assets/Scripts/NoiseMapGeneration.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class NoiseMapGeneration : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Method that will return an array of Cartesian coordinates representing a noise map
+    // Noise values are calculated using the Perlin Noise function
+    // mapDepth: size along the z-axis of the map
+    // mapWidth: size along the x-axis of the map
+    public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, int scale)
     {
-        
-    }
+        // Initialise noiseMap object to contain each coordinate in the map
+        float[,] noiseMap = new float[mapDepth, mapWidth];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int z = 0; z < mapDepth; z++)
+            for (int x = 0; x < mapWidth; x++)
+            {
+                // Calculate sample indices based on the coordinates and the scale
+                float sampleX = x / scale;
+                float sampleZ = z / scale;
+
+                float noise = Mathf.PerlinNoise(sampleX, sampleZ);
+
+                noiseMap[z, x] = noise;
+            }
+
+        return noiseMap;
     }
 }
